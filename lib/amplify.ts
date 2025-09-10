7a9969345a94bf6d63e869f59499271f79e9baae
davo <davcho2014@gmail.com>
2025-09-10 14:44:29 +0100

    Modify amolify.ts

--- lib/amplify.ts
+++ lib/amplify.ts
@@ -1,3 +1,46 @@
+// 'use client';
+
+// import { Amplify } from 'aws-amplify';
+
+// // Only configure if we're in the browser and have the required env vars
+// if (typeof window !== 'undefined') {
+//   const userPoolId = process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_ID;
+//   const userPoolClientId = process.env.NEXT_PUBLIC_AWS_COGNITO_CLIENT_ID;
+//   const region = process.env.NEXT_PUBLIC_AWS_REGION;
+
+//   if (userPoolId && userPoolClientId && region) {
+//     const amplifyConfig = {
+//       Auth: {
+//         Cognito: {
+//           userPoolId: userPoolId,
+//           userPoolClientId: userPoolClientId,
+//           loginWith: {
+//             email: true,
+//           },
+//           signUpVerificationMethod: 'code' as const,
+//           userAttributes: {
+//             email: {
+//               required: true,
+//             },
+//           },
+//           allowGuestAccess: false,
+//           passwordFormat: {
+//             minLength: 8,
+//             requireLowercase: true,
+//             requireUppercase: true,
+//             requireNumbers: true,
+//             requireSpecialCharacters: true,
+//           },
+//         },
+//       },
+//     };
+
+//     Amplify.configure(amplifyConfig);
+//     console.log('Amplify configured with:', { userPoolId, userPoolClientId, region });
+//   } else {
+//     console.error('Missing Amplify configuration:', { userPoolId, userPoolClientId, region });
+//   }
+
 'use client';
 
 import { Amplify } from 'aws-amplify';
@@ -14,23 +57,6 @@ if (typeof window !== 'undefined') {
         Cognito: {
           userPoolId: userPoolId,
           userPoolClientId: userPoolClientId,
-          loginWith: {
-            email: true,
-          },
-          signUpVerificationMethod: 'code' as const,
-          userAttributes: {
-            email: {
-              required: true,
-            },
-          },
-          allowGuestAccess: false,
-          passwordFormat: {
-            minLength: 8,
-            requireLowercase: true,
-            requireUppercase: true,
-            requireNumbers: true,
-            requireSpecialCharacters: true,
-          },
         },
       },
     };
